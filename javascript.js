584125815bc2393c21041f69caee10f048d9c88f
Jimmy Wu <jimmywu063@gmail.com>
2024-08-22 00:44:04 -0700

    Added row5 containing equal button
    Seperated displayable to numbers and operators and created notes for next work session
    
    Styled equal button to take up entire width of container

--- javascript.js
+++ javascript.js
@@ -41,14 +41,37 @@ const operate = (oper, num1, num2) => {
 }
 
 const displayable = document.querySelectorAll("#displayable");
+const operators = document.querySelectorAll("#operator");
+
+// we can know which num variable we assigning to by checking if the operator type undefined or not (num1 is undefinded, num2 if defined)
 displayable.forEach((number) => {
-    let num = number.className;
     number.addEventListener("click", () => {
-        display.value += num;
+        display.value += number.className;
+        displayValue = display.value;
     });
 });
+
+//should also have some type of boolean expresion that tells calculator we move onto num2 when clicking an operator
+// idea maybe make operator an object that holds the operator type and bool if operator has been chosen
+// actually we can know if operator has been clicked by knowing the operator type, if same type is clicked then we will set it back to undefined
+operators.forEach((oper) => {
+    oper.addEventListener("click", () => {
+        display.value += oper.className;
+        displayValue = display.value;
+        operator = oper.className.trim();
+    });  
+})
 
 const clear = document.querySelector(".clear");
 clear.addEventListener("click", () => {
     display.value = "";
+    displayValue = display.value;
+    operator = undefined;
 });
+
+/* To do
+   - Make calculator behave more like phone calculator (similar to student example)
+   - After clicking operator button, calculator must know that you are now entering number into num2
+   - Clicking an operator when we are looking at num2 will run operate()
+   
+*/
